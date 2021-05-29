816ba41f0ea5b59aa96bea6bc0cf3c22a723523f
alperenatikhan <alperenmyung@gmail.com>
2021-05-29 22:25:49 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -14,13 +14,21 @@ export default function App() {
         <div id="nav-wrapper" className="navbar-col-right">
           <ul>
             <div className="nav-items">
-              <li> <i className="fas fa-id-card"></i>Summary</li>
+              <li>
+                {' '}
+                <i className="fas fa-id-card" />Summary
+              </li>
             </div>
             <div className="nav-items">
-              <li> <i className="fas fa-store"></i>Portfolio</li>
+              <li>
+                {' '}
+                <i className="fas fa-store" />Portfolio
+              </li>
             </div>
             <div className="nav-items">
-              <li><i className="fas fa-globe"></i>Contact</li>
+              <li>
+                <i className="fas fa-globe" />Contact
+              </li>
             </div>
           </ul>
         </div>
@@ -39,11 +47,112 @@ export default function App() {
             </div>
           </div>
           <div id="text-space">
-            <p> I am a <a href="https://uni-flensburg.academia.edu/AlperenAtik" className="text-link" target="blank">Ph.D candidate </a> in Sociology, <a href="https://www.opendemocracy.net/en/author/alperen-atik/" className="text-link" target="blank"> Independent Journalist </a> and a budding coder. During the research for my doctoral dissertation, I had to analyse more than four hundred documents. For easing my research, I wrote a webscraper with a  <a href="https://replit.com/@AlperenAtik/parklarbizimfinder" className="text-link" target="blank">keyword retrieval</a> function. For coding my first webscraper, I learnt Python by following the tutorials in <a href="https://www.codecademy.com/" className="text-link" target="blank">CodeCademy</a>. Gradually, I have coded webscrapers for <a href="https://replit.com/@AlperenAtik/Tandf-explorer-1" className="text-link" target="blank">literature</a> and <a href="https://replit.com/@AlperenAtik/Wiki-references-scraper" target="blank" className="text-link"> archive</a> research for several <a href="https://replit.com/@AlperenAtik/linkspr-explorer" className="text-link" target="blank"> academic journals</a>, <a href="https://replit.com/@AlperenAtik/Bianet-English-Archive-Search" className="text-link" target="blank">news portals</a> and <a href="https://replit.com/@AlperenAtik/The-New-Yorker" className="text-link"> popular magazines</a>.
+            <p>
+              {' '}
+              I am a{' '}
+              <a
+                href="https://uni-flensburg.academia.edu/AlperenAtik"
+                className="text-link"
+                target="blank"
+              >
+                Ph.D candidate{' '}
+              </a>{' '}
+              in Sociology,{' '}
+              <a
+                href="https://www.opendemocracy.net/en/author/alperen-atik/"
+                className="text-link"
+                target="blank"
+              >
+                {' '}
+                Independent Journalist{' '}
+              </a>{' '}
+              and a budding coder. During the research for my doctoral
+              dissertation, I had to analyse more than four hundred documents.
+              For easing my research, I wrote a webscraper with a{' '}
+              <a
+                href="https://replit.com/@AlperenAtik/parklarbizimfinder"
+                className="text-link"
+                target="blank"
+              >
+                keyword retrieval
+              </a>{' '}
+              function. For coding my first webscraper, I learnt Python by
+              following the tutorials in{' '}
+              <a
+                href="https://www.codecademy.com/"
+                className="text-link"
+                target="blank"
+              >
+                CodeCademy
+              </a>
+              . Gradually, I have coded webscrapers for{' '}
+              <a
+                href="https://replit.com/@AlperenAtik/Tandf-explorer-1"
+                className="text-link"
+                target="blank"
+              >
+                literature
+              </a>{' '}
+              and{' '}
+              <a
+                href="https://replit.com/@AlperenAtik/Wiki-references-scraper"
+                target="blank"
+                className="text-link"
+              >
+                {' '}
+                archive
+              </a>{' '}
+              research for several{' '}
+              <a
+                href="https://replit.com/@AlperenAtik/linkspr-explorer"
+                className="text-link"
+                target="blank"
+              >
+                {' '}
+                academic journals
+              </a>
+              ,{' '}
+              <a
+                href="https://replit.com/@AlperenAtik/Bianet-English-Archive-Search"
+                className="text-link"
+                target="blank"
+              >
+                news portals
+              </a>{' '}
+              and{' '}
+              <a
+                href="https://replit.com/@AlperenAtik/The-New-Yorker"
+                className="text-link"
+              >
+                {' '}
+                popular magazines
+              </a>
+              .
             </p>
 
-            <p> My web-developer adventure started after completing freecodecamp's "Responsive Design" certificate. The first web app I designed was a <a href="https://palegreenstimulatingdos--alperenatik.repl.co/" target="blank" className="text-link">Flask website</a> which presents results from my webscrapers.Currently, I am learning ReactJs and preparing projects for freecodecamp's "Front End Developer" certificate. </p>
-           
+            <p>
+              {' '}
+              My web-developer adventure started after completing freecodecamp's
+              "Responsive Design" certificate. The first web app I designed was
+              a{' '}
+              <a
+                href="https://palegreenstimulatingdos--alperenatik.repl.co/"
+                target="blank"
+                className="text-link"
+              >
+                Flask website
+              </a>{' '}
+              which presents results from my webscrapers.Currently, I am
+              learning ReactJs and preparing{' '}
+              <a
+                href="https://codepen.io/alperen-atik/"
+                target="blank"
+                className="text-link"
+              >
+                projects
+              </a>{' '}
+              for freecodecamp's "Front End Developer" certificate.{' '}
+            </p>
           </div>
         </div>
       </section>
